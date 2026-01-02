b74482ba9882b1f5283fbc242fa6d94204903b03
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-02 19:13:46 +0700

    add destinasi untuk tambah siswa

---
+++ app/src/main/java/com/example/questfirebase/view/route/DestinasiEntry.kt
@@ -0,0 +1,7 @@
+package com.example.questfirebase.view.route
+
+import com.example.questfirebase.R
+object DestinasiEntry: DestinasiNavigasi {
+    override val route ="tambah_siswa"
+    override val titleRes = R.string.entry_siswa
+}
