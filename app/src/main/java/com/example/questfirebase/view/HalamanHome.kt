aa4918596d8504ff11ee285820f205140563dd5f
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-02 21:22:25 +0700

    membuat fun ItemSiswa untuk menampilkan card yang disusun secara colum yang berisi nama, telpon dan alamat,

--- app/src/main/java/com/example/questfirebase/view/HalamanHome.kt
+++ app/src/main/java/com/example/questfirebase/view/HalamanHome.kt
@@ -1,26 +1,39 @@
 package com.example.questfirebase.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questfirebase.view.route.DestinasiHome
 import com.example.questfirebase.view.viewmodel.HomeViewModel
 import com.example.questfirebase.view.viewmodel.PenyediaViewModel
 import com.example.questfirebase.R
+import com.example.questfirebase.modeldata.Siswa
+import com.example.questfirebase.view.viewmodel.StatusUiSiswa
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -62,5 +75,46 @@ fun HalamanHome(
                 .padding(isiRuang)
                 .fillMaxSize()
         )
+    }
+}
+
+
+
+@Composable
+fun ItemSiswa(
+    siswa: Siswa,
+    modifier: Modifier= Modifier
+){
+    Card(
+        modifier=modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column (
+            modifier= Modifier
+                .padding(dimensionResource(R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id=R.dimen.padding_small))
+        ){
+            Row(
+                modifier= Modifier.fillMaxWidth()
+            ){
+                Text(
+                    text= siswa.nama,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text=siswa.telpon,
+                    style= MaterialTheme.typography.bodyMedium
+                )
+            }
+            Text(
+                text = siswa.alamat,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
     }
 }
