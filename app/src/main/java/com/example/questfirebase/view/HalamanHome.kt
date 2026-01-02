825e6e03d40d29492b507e80c12dc7718fdaefb8
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-02 21:04:51 +0700

    add halaman Home yang berisi topbar (untuk judul) dan icon untuk tambah

---
+++ app/src/main/java/com/example/questfirebase/view/HalamanHome.kt
@@ -0,0 +1,57 @@
+package com.example.questfirebase.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questfirebase.view.route.DestinasiHome
+import com.example.questfirebase.view.viewmodel.HomeViewModel
+import com.example.questfirebase.view.viewmodel.PenyediaViewModel
+import com.example.questfirebase.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanHome(
+    navigateToItemEntry:()->Unit,
+    navigatetoItemUpdate:(Int)-> Unit,
+    modifier: Modifier=Modifier,
+    viewModel: HomeViewModel= viewModel(factory = PenyediaViewModel.factory)
+){
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    Scaffold (
+        modifier = modifier .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior=scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        }
+    ){
+
+    }
+}
