c5541f180a61cdc8218d7752547bcd6e6bbfd1cb
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-02 22:26:00 +0700

    membuat fun untuk formulir tambah siswa yang dibungkus dalam column berisi field untuk nama, alamat dan telpon(telpon menggunakan keyboard yang angka aja)

--- app/src/main/java/com/example/questfirebase/view/HalamanEntry.kt
+++ app/src/main/java/com/example/questfirebase/view/HalamanEntry.kt
@@ -1,21 +1,30 @@
 package com.example.questfirebase.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questfirebase.view.viewmodel.EntryViewModel
 import com.example.questfirebase.view.viewmodel.PenyediaViewModel
 import com.example.questfirebase.R
+import com.example.questfirebase.modeldata.DetailSiswa
 import com.example.questfirebase.view.route.DestinasiEntry
 import kotlinx.coroutines.launch
 
@@ -55,5 +64,55 @@ fun HalamanEntry(
                 .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun FormTambahSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier= Modifier,
+    onValueChange:(DetailSiswa)->Unit ={},
+    enabled: Boolean=true
+){
+    Column (
+        modifier=modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+    ){
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange= {onValueChange(detailSiswa.copy(nama = it))},
+            label = { Text(stringResource(R.string.nama)) },
+            modifier= Modifier.fillMaxWidth(),
+            enabled=enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.alamat,
+            onValueChange= {onValueChange(detailSiswa.copy(alamat = it))},
+            label = { Text(stringResource(R.string.alamat)) },
+            modifier= Modifier.fillMaxWidth(),
+            enabled=enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.telpon,
+            onValueChange= {onValueChange(detailSiswa.copy(telpon = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(stringResource(R.string.telpon)) },
+            modifier= Modifier.fillMaxWidth(),
+            enabled=enabled,
+            singleLine = true
+        )
+         if(enabled){
+             Text(
+                 text = stringResource(R.string.required_field),
+                 modifier= Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
+             )
+         }
+        Divider(
+            thickness = dimensionResource(R.dimen.padding_small),
+            modifier = Modifier .padding(bottom = dimensionResource(R.dimen.padding_medium))
+        )
+
     }
 }
