8ab46718622ae1dfd334bb1a1749b395d2e935b7
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-02 18:37:44 +0700

    menambahkan private fun untuk validasiInput jika field form nya kosong

--- app/src/main/java/com/example/questfirebase/view/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questfirebase/view/viewmodel/EntryViewModel.kt
@@ -4,12 +4,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.questfirebase.modeldata.DetailSiswa
 import com.example.questfirebase.modeldata.UIStateSiswa
 import com.example.questfirebase.repositori.RepositorySiswa
 
 class EntryViewModel(private  val repositorySiswa: RepositorySiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private fun validasiInput(uiState: DetailSiswa=uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank()&& alamat.isNotBlank()&&telpon.isNotBlank()
+        }
+    }
 
 
 }
