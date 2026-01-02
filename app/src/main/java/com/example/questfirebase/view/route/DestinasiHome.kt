3eb6629ce02ab0ab791feac9b37811409de1f951
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-02 19:04:04 +0700

    add destinasi untuk home

---
+++ app/src/main/java/com/example/questfirebase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebase.view.route
+
+import com.example.questfirebase.R
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
