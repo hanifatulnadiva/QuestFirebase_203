4eae3f2e09421c2be6384581792e979863507b99
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-08 15:56:18 +0700

    add fun HalamanEdit yang disusun secara scaffold

--- app/src/main/java/com/example/questfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questfirebase/view/HalamanEdit.kt
@@ -1,4 +1,24 @@
 package com.example.questfirebase.view
 
-class HalamanEdit {
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questfirebase.view.viewmodel.EditViewModel
+import com.example.questfirebase.view.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanEdit(
+    navigationBack:()-> Unit,
+    onNavigateUp:()->Unit,
+    modifier:Modifier= Modifier,
+    viewModel: EditViewModel= viewModel(factory = PenyediaViewModel.factory)
+
+) {
+    Scaffold (
+
+    ){  }
+
 }
