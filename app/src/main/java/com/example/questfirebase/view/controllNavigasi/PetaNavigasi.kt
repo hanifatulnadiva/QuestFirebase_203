dfb9bc6c76a9d2c70463219864b278d3bc773ddc
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-08 16:13:20 +0700

    add navigasi untuk halaman detail

--- app/src/main/java/com/example/questfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/questfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -3,14 +3,19 @@ package com.example.questfirebase.view.controllNavigasi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.questfirebase.view.HalamanDetailSiswa
+import com.example.questfirebase.view.HalamanEdit
 import com.example.questfirebase.view.HalamanEntry
 import com.example.questfirebase.view.HalamanHome
 import com.example.questfirebase.view.route.DestinasiDetail
 import com.example.questfirebase.view.route.DestinasiEntry
 import com.example.questfirebase.view.route.DestinasiHome
+import com.example.questfirebase.view.route.DestinasiUpdate
 
 @Composable
 fun DataSiswaApp(
@@ -51,5 +56,13 @@ fun HostNavigasi(
                 }
             )
         }
+
+        composable (DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail.itemIdArg){
+            type= NavType.StringType
+        })){
+            HalamanDetailSiswa(navigateToEditItem = {navController.navigate("${DestinasiUpdate.route}/$it")},
+                navigateBack = {navController.navigate(DestinasiHome.route)})
+        }
+
     }
 }
