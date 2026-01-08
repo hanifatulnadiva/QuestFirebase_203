f9100d8f787dcbbba116f1d92d2e6ea4d3ed9d10
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-08 09:37:11 +0700

    membuat sealed interface untuk StatusUIDetail yang terdiri atas data class success, object error dan loading

---
+++ app/src/main/java/com/example/questfirebase/view/viewmodel/DetailViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.questfirebase.view.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questfirebase.modeldata.Siswa
+import com.example.questfirebase.repositori.RepositorySiswa
+import com.example.questfirebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail{
+    data class Success(val satusiswa: Siswa?):StatusUIDetail
+    object Error: StatusUIDetail
+    object Loading: StatusUIDetail
+}
