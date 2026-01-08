cb38d50bc31d7e677df419c880cecdbda433d6f0
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-01-08 16:02:18 +0700

    add body untuk halaman edit yang memanggil fun dari EntrySiswaBody

--- app/src/main/java/com/example/questfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questfirebase/view/HalamanEdit.kt
@@ -1,14 +1,17 @@
 package com.example.questfirebase.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questfirebase.view.route.DestinasiUpdate
 import com.example.questfirebase.view.viewmodel.EditViewModel
 import com.example.questfirebase.view.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,7 +29,22 @@ fun HalamanEdit(
                 canNavigateBack = true,
                 navigateUp = onNavigateUp
             )
-        }
-    ){  }
+        },
+        modifier = modifier
+    ){
+        innerPadding->
+        val corountineScope= rememberCoroutineScope ()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                corountineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigationBack()
+                }
+            },
+            modifier= Modifier.padding(innerPadding)
+        )
+    }
 
 }
